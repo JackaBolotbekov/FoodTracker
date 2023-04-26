c13cebc9b07b38bb2c59e704bf125391e4a214ef
JackaBolotbekov <jackabolotbekow@gmail.com>
2023-04-27 01:18:33 +0600

    make AddDefaultFragment used SafeArgs

--- presentation/src/main/java/com/example/presentation/ui/fragments/editdefault/EditDefaultFragment.kt
+++ presentation/src/main/java/com/example/presentation/ui/fragments/editdefault/EditDefaultFragment.kt
@@ -1,17 +1,31 @@
 package com.example.presentation.ui.fragments.editdefault
 
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.presentation.R
 import com.example.presentation.base.BaseFragment
-import com.example.presentation.databinding.FragmentHistoryBinding
-import com.example.presentation.ui.fragments.history.HistoryViewModel
+import com.example.presentation.databinding.FragmentEditDefaultBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class EditDefaultFragment :
-    BaseFragment<FragmentHistoryBinding, HistoryViewModel>(R.layout.fragment_edit_default) {
+    BaseFragment<FragmentEditDefaultBinding, EditDefaultViewModel>(R.layout.fragment_edit_default) {
 
-    override val binding by viewBinding(FragmentHistoryBinding::bind)
-    override val viewModel: HistoryViewModel by viewModels()
+    override val binding by viewBinding(FragmentEditDefaultBinding::bind)
+    override val viewModel: EditDefaultViewModel by viewModels()
+
+    override fun setupObserves() {
+        setUpListener()
+    }
+
+    private fun setUpListener() = with(binding) {
+        swoosh.setOnClickListener {
+            val text = etEditNormsDay.text.toString().trim()
+            if (text.length < 5) {
+                val action = EditDefaultFragmentDirections.actionEditDefaultFragmentToHomeFragment(text.toInt())
+                findNavController().navigate(action)
+            } else {etEditNormsDay.error = "Вы явно переоцениваете свои возможности!"}
+        }
+    }
 }
